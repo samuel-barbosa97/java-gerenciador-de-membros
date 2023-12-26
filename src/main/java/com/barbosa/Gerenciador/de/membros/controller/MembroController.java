0a834708783f355c7e127c917bc21d477940afe2
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2023-12-26 17:54:19 -0300

    Adicionando HttpStatus

--- src/main/java/com/barbosa/Gerenciador/de/membros/controller/MembroController.java
+++ src/main/java/com/barbosa/Gerenciador/de/membros/controller/MembroController.java
@@ -5,35 +5,49 @@ import com.barbosa.Gerenciador.de.membros.model.UpdateMembro;
 import com.barbosa.Gerenciador.de.membros.service.MembroService;
 import io.swagger.annotations.Api;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/membros")
-@Api(tags = "Membros", description = "Operações relacionadas a membros")
+@Api(tags = "Membros", value = "Operações relacionadas a membros")
 @AllArgsConstructor
 public class MembroController {
 
     private MembroService membroService;
 
     @PostMapping
-    public Membro saveMembro(@RequestBody Membro membro) {
-        return membroService.saveMembro(membro);
+    public ResponseEntity<Membro> saveMembro(@RequestBody Membro membro) {
+        Membro savedMembro = membroService.saveMembro(membro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedMembro);
     }
 
     @GetMapping
-    public List<Membro> listMembros() {
-        return membroService.listarMembros();
+    public ResponseEntity<List<Membro>> listMembros() {
+        List<Membro> membros = membroService.listarMembros();
+        return ResponseEntity.status(HttpStatus.OK).body(membros);
     }
 
     @PutMapping("/{id}")
-    public Membro updateMembro(@RequestBody UpdateMembro updateMembro, @PathVariable("id") String id) throws Exception {
-        return membroService.updateMembro(updateMembro, id);
+    public ResponseEntity<Membro> updateMembro(@RequestBody UpdateMembro updateMembro, @PathVariable("id") String id) {
+        try {
+            Membro updatedMembro = membroService.updateMembro(updateMembro, id);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedMembro);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteMembro(@PathVariable("id") String id) {
-        membroService.deleteMembro(id);
+    public ResponseEntity<Void> deleteMembro(@PathVariable("id") String id) {
+        try {
+            membroService.deleteMembro(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
