68c16a996447349f63070813f8a3c514f71eecef
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2023-12-26 09:55:08 -0300

    Projeto criado

---
+++ src/main/java/com/barbosa/Gerenciador/de/membros/GerenciadorDeMembrosApplication.java
@@ -0,0 +1,14 @@
+package com.barbosa.Gerenciador.de.membros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeMembrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeMembrosApplication.class, args);
+		System.out.println("Projeto rodando");
+	}
+
+}
---
+++ src/test/java/com/barbosa/Gerenciador/de/membros/GerenciadorDeMembrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbosa.Gerenciador.de.membros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeMembrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
